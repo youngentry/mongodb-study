ab169f75709392c44cc8a5f5dc8b1b76b378641e
unknown <gentry_@naver.com>
2023-01-01 04:07:40 +0900

    feat: mongoDB 게시물 번호 부여하기

--- server 필기용.js
+++ server 필기용.js
@@ -1,6 +1,12 @@
 const express = require("express"); // 라이브러리를 쓸게요.
 const app = express(); // express 객체 생성할게요.
 app.use(express.urlencoded({ extended: true }));
+// 221231 19:20 EJS 서버에서 가져온 데이터 html에 쉽게 뿌리기
+// ejs 라이브러리를 설치합니다.
+// $ npm i --save-dev ejs
+// 아래와 같이 ejs를 view engine으로 등록합니다.
+app.set("view engine", "ejs");
+
 const PORT = 8080;
 
 // 221231 03:30 MongoDB 연결방법
@@ -8,7 +14,7 @@ const PORT = 8080;
 const mongoUri = `mongodb+srv://sys0321:max0321@cluster0.lncdty3.mongodb.net/todoapp?retryWrites=true&w=majority`;
 // MongoClient 모듈을 가져옵니다.
 const { MongoClient } = require("mongodb");
-// MongoClient class를 생성합니다.
+// MongoClient class를 생성합니다. 두번째 인자는 중요하지 않은 warning을 제거해준다 합니다.
 const client = new MongoClient(mongoUri, { useUnifiedTopology: true });
 // 변수를 하나 생성합니다.
 let db;
@@ -18,11 +24,11 @@ client.connect((에러) => {
   if (에러) return console.log(에러);
 
   // 221231 02:30 데이터베이스에 연결하기
-  // 아래와 똑같이 입력합니다.
+  // todoapp 데이터베이스에 접속합니다.
   db = client.db("todoapp");
 
-  // post collection에 데이터를 저장합니다.
-  db.collection("post").insertOne({ 이름: "You", nationality: "KOR" }, (에러, 결과) => {
+  // post collection에 데이터를 저장합니다. id는 _언더바를 붙여주어야 합니다.
+  db.collection("post").insertOne({ _id: 1, 이름: "You", nationality: "KOR" }, (에러, 결과) => {
     console.log("저장완료");
   });
 
@@ -53,5 +59,38 @@ app.get("/", (요청, 응답) => {
 // 값을 가져오려면 상단에 app.use(express.urlencoded({extended: true})) 추가
 app.post("/경로", (요청, 응답) => {
   응답.send("/add로 POST 전송 성공했다");
-  console.log(요청.body.title);
+});
+
+// 221231 19:10 post 요청을 통해서 유저가 입력한 정보를 데이터베이스에 저장하려면 이렇게
+app.post("/add", (요청, 응답) => {
+  app.post("/add", (요청, 응답) => {
+    응답.send(`/add로 POST 전송 성공했다.`);
+    // 230101 03:30 counter collection에서 {name:'게시물수'} 를 찾고 싶다.
+    db.collection("counter").findOne({ name: "게시물수" }, (에러, 결과) => {
+      const 총게시물갯수 = 결과.totalPost;
+
+      db.collection("post").insertOne({ _id: 총게시물갯수, 제목: 요청.body.title, 날짜: 요청.body.date }, (에러, 결과) => {
+        console.log("저장완료");
+        // 230101 03:30 데이터 수정하기
+        // 결과.totalPost 의 값을 +1 해주어야 다음에 생성할 때 id가 겹치지 않는다
+        // $set 연산자: 바꿔주세요. $inc: 증가시켜주세요
+        db.collection("counter").updateOne({ 수정할데이터 }, { $inc: { 수정값 } }, (에러, 결과) => {
+          if (에러) return console.log(에러);
+        });
+      });
+    });
+  });
+});
+
+// 221231 19:32 데이터베이스에서 받아온 자료 넣어주려면 render
+app.get("/list", (요청, 응답) => {
+  // 모든 데이터 가져오기
+  db.collection("post")
+    .find()
+    .toArray((에러, 결과) => {
+      console.log(결과);
+      // => post collection에서 모든 데이터를 찾아 array 형태로 담은 결과를
+      // list.ejs 파일을 렌더할 때 {보낼데이터이름: 결과} 형태로 데이터를 전달해 주세요.
+      응답.render("list.ejs", { posts: 결과 });
+    });
 });
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-const PORT = 8080;
+app.set("view engine", "ejs");
 
+const PORT = 8080;
 const mongoUri = `mongodb+srv://sys0321:max0321@cluster0.lncdty3.mongodb.net/todoapp?retryWrites=true&w=majority`;
+
 const { MongoClient } = require("mongodb");
 const client = new MongoClient(mongoUri, { useUnifiedTopology: true });
 var db;
@@ -13,7 +15,21 @@ client.connect((에러) => {
   db = client.db("todoapp");
 
   app.listen(8080, function () {
-    console.log("listening on 8080");
+    console.log("8080 포트 tooapp Database에 연결 되었음");
+  });
+});
+
+app.post("/add", (요청, 응답) => {
+  응답.send(`/add로 POST 전송 성공했다.`);
+  db.collection("counter").findOne({ name: "게시물수" }, (에러, 결과) => {
+    const 총게시물갯수 = 결과.totalPost;
+
+    db.collection("post").insertOne({ _id: 총게시물갯수, 제목: 요청.body.title, 날짜: 요청.body.date }, (에러, 결과) => {
+      console.log("저장완료");
+      db.collection("counter").updateOne({ name: "게시물수" }, { $inc: { totalPost: 1 } }, (에러, 결과) => {
+        if (에러) return console.log(에러);
+      });
+    });
   });
 });
 
@@ -29,7 +45,10 @@ app.get("/write", (요청, 응답) => {
   응답.sendFile(__dirname + "/write.html");
 });
 
-app.post("/add", (요청, 응답) => {
-  응답.send(`/add로 POST 전송 성공했다. 요청.body.title = ${요청.body.title}`);
-  console.log(요청.body.title);
+app.get("/list", (요청, 응답) => {
+  db.collection("post")
+    .find()
+    .toArray((에러, 결과) => {
+      응답.render("list.ejs", { posts: 결과 });
+    });
 });
